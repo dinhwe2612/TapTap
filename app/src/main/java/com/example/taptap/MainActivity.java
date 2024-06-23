f9ada665ad424d59896b6551a9a507180ffefe07
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2024-06-23 23:10:20 +0700

    Add BottomMenuBar and corresponding Fragments

--- app/src/main/java/com/example/taptap/MainActivity.java
+++ app/src/main/java/com/example/taptap/MainActivity.java
@@ -7,18 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.taptap.databinding.ActivityMainBinding;
+import com.example.taptap.home.HomeFragment;
+import com.example.taptap.message.MessageFragment;
+import com.example.taptap.profile.ProfileFragment;
+import com.example.taptap.shop.ShopFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setBackground(null);
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int idItem = item.getItemId();
+            if (idItem == R.id.home_menu) {
+                replaceFragment(new HomeFragment());
+            } else if (idItem == R.id.shopping_menu) {
+                replaceFragment(new ShopFragment());
+            } else if (idItem == R.id.message_menu) {
+                replaceFragment(new MessageFragment());
+            } else if (idItem == R.id.profile_menu) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
